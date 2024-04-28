bc21cca270e377210a5afc013da6753b6f061765
neerajadsul <neeraj.adsul@gmail.com>
2024-04-28 13:28:33 +0100

    A reference design for LLM interaction.

---
+++ llms/llms.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+
+class ChatAssistant(ABC):
+    """Abstract baseclass to support variety of LLMs."""
+    @abstractmethod
+    def get_completion(self, raw_prompt):
+        """LLM API specific completion method.
+        Args:
+            raw_prompt: raw text template containing fields to replace.
+        """
+        pass
+
+    @abstractmethod
+    def prepare_prompt(self, *args, **kwargs):
+        """Final prompt to be sent to the completion API."""
+        pass
+
+    @abstractmethod
+    def get_message(self, response):
+        """Return message only part from completion response.
+
+        Args:
+            response: full response from LLM API, typically in JSON format.
+        """
+        pass
+
+    @abstractmethod
+    def log_response(self, response):
+        """Save entire response in the datastore including traceability info 
+        and message for dissimination later.
+
+        Args:
+            response: full response from LLM API, typically in JSON format.
+        """
+        pass
