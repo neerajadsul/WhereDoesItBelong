f05215ca75d13cdef43b811e576a0d1b0cf4630d
neerajadsul <neeraj.adsul@gmail.com>
2024-04-28 23:52:17 +0100

    Logging for debug and traces.

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
+import logging
+
 from flask import Flask, request
-from prediction import predict
+from prediction import Prediction
 
 app = Flask(__name__)
+
+prediction = Prediction()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
 
 @app.route('/classify', methods=['POST'])
 def classify():
     data = request.data.decode()
-    result = predict(data)
-    return result
+    result = prediction.predict(data)
+    return prediction.format_result(result)
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(port=8000, debug=False)
