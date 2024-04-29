331e8a96692b3e50db3940826d9eed135ac16b27
neerajadsul <neeraj.adsul@gmail.com>
2024-04-29 10:34:24 +0100

    Easier to save filename with nanoseconds as name.

--- llms/datastores.py
+++ llms/datastores.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 import os
-import datetime
+import time
 
 class Datastore(ABC):
     """Ability to store data in specific sink."""
@@ -18,17 +18,15 @@ class LogFiles(Datastore):
         """
         if not os.path.exists(log_path):
             os.makedirs(log_path)
-        
         self.log_path = log_path
         
     def save(self, response, filename=None):
         if filename is None:
-            filename = datetime.time().strftime('%Y%m%d-%H%M%S-%f_response.json')
+            filename = f'{int(time.time())}_response.json'
         filepath = os.path.join(self.log_path, filename)
         with open(filepath, 'wt') as fp:
             fp.write(response)
-            
+
 
 if __name__ == "__main__":
     ds = LogFiles('./temp/log1.json')
-    
